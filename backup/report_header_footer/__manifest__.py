7cb076266bd35a62bd6a69b1af7bdd23c990afb1
rocky <ehtishamfaisal@gmail.com>
2017-11-08 13:26:49 +0500

    [REM] Backup dir removed

--- backup/report_header_footer/__manifest__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-{
-    'name': "Report Header Footer",
-    'description': "Report Header Footer",
-    'author': 'Muhammad Kamran',
-    'application': True,
-    'depends': ['base_setup','report'],
-    'data': ['views/template.xml'],
-}
